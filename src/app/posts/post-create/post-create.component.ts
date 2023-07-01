ba5aa0302778b84281cac1db9f65019ab68dfa56
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-01 19:31:09 +0530

    post component added

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.scss']
+})
+export class PostCreateComponent {
+
+}
