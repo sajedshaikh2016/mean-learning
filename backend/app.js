5b76de56d1117c6cb4877984c94eece708c6b9ce
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-07 22:33:20 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use((req, res, next) => {
-    console.log("First middleware.");
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!')
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: 'sfsd423423',
+            title: "First server-side post",
+            content: 'This is coming from the server'
+        },
+        {
+            id: 'jtyrrtjty',
+            title: "Second server-side post",
+            content: 'This is coming from the server'
+        },
+        {
+            id: '35hfggh',
+            title: "Third server-side post",
+            content: 'This is coming from the server'
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched sucessfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
