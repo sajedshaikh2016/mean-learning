ebe9daf9284df6f061442ead4381952323c25800
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-07 22:54:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+});
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         {
