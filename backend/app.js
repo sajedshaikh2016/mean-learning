4269681b16911101c43266d652f1fcabfc8f28e5
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-08 17:11:42 +0530

    Code refactoring done

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
+
 
 const app = express();
 const port = 8000;
@@ -19,7 +20,6 @@ mongoose.connect("mongodb+srv://sajedshaikh:eNnISogWd0d8qqQl@cluster0.1csszq1.mo
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -27,61 +27,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save().then((createdPost) => {
-        res.status(201).json({
-            message: 'Post added sucessfully!',
-            postId: createdPost._id
-        });
-    });
-
-});
-
-app.put('/api/posts:id', (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({ _id: req.params.id }, post).then((result) => {
-        res.status(200).json({ message: 'Posts updated sucessfully!' });
-    });
-});
-
-
-app.get('/api/posts', (req, res, next) => {
-    Post.find()
-        .then((documents) => {
-            res.status(200).json({
-                message: 'Posts fetched sucessfully!',
-                posts: documents
-            });
-        });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-    Post.findById(req.params.id)
-        .then((post) => {
-            if (post) {
-                res.status(200).json(post);
-            } else {
-                res.status(404).json({
-                    message: 'Post not found!'
-                });
-            }
-        });
-});
-
-
-app.delete('/api/posts/:id', (req, res, next) => {
-    console.log("req.params.id", req.params.id);
-    Post.deleteOne({ _id: req.params.id }).then((result) => {
-        res.status(200).json({ message: 'Post deleted!' });
-    });
-});
+app.use(postsRoutes);
 
 module.exports = app;
